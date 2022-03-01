5d09570f7554e743266af2ec370c0714918551f4
Kevin Baumgartner <kevjbaumgartner@gmail.com>
2022-02-28 20:52:29 -0500

    More landing style changes

--- app/src/components/Landing.js
+++ app/src/components/Landing.js
@@ -8,23 +8,18 @@ export default function Landing() {
 				<img src='https://media-exp1.licdn.com/dms/image/C4D03AQH8VzQ649Vvyg/profile-displayphoto-shrink_800_800/0/1545340413878?e=1651104000&v=beta&t=_8KRLDVm7oqysz90sZoDaK1qC8RJYWanBCnffp-JagE' alt='logo'></img>
 			</div>
 			<div id='menu'>
-				<p className='text'>
-					Hey, nice to meet you!
-				</p>
-				<p className='text'>
-					I'm <span className='focus'>Kevin Baumgartner</span>,
+				<div className='text'>
+					Hi! I'm <span className='focus'>Kevin Baumgartner</span>,
 					<br />
 					a full stack developer.
-				</p>
-				<p className='text'>
-					I specialize in web development,
-					<br />
-					using the <a href='https://www.mongodb.com/mern-stack' target='_blank' className='focus-link'>MERN solution stack</a> to create beautiful solutions for everyday situtions.
-				</p>
-				<div id='links-social'>
-					<a href='https://github.com/kevjbaumgartner/' target='_blank' rel='noreferrer'><i className='fa-brands fa-github-square'></i></a>
-					<a href='https://www.linkedin.com/in/kevjbaumgartner/' target='_blank' rel='noreferrer'><i className='fa-brands fa-linkedin'></i></a>
-					<a href='mailto:kevjbaumwork@gmail.com'><i className='fa-solid fa-square-envelope'></i></a>
+					<br /><br />
+					I specialize in web development, using the <a href='https://www.mongodb.com/mern-stack' target='_blank' className='focus-link'>MERN solution stack</a> to create beautiful solutions for everyday situtions.
+					<br /><br />
+					<div id='links-social'>
+						<a href='https://github.com/kevjbaumgartner/' target='_blank' rel='noreferrer'><i className='fa-brands fa-github-square'></i></a>
+						<a href='https://www.linkedin.com/in/kevjbaumgartner/' target='_blank' rel='noreferrer'><i className='fa-brands fa-linkedin'></i></a>
+						<a href='mailto:kevjbaumwork@gmail.com'><i className='fa-solid fa-square-envelope'></i></a>
+					</div>
 				</div>
 			</div>
 		</div>
