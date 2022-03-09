16a9b716f389f252871f0e0603c639307b892a06
Kevin Baumgartner <kevjbaumgartner@gmail.com>
2022-03-08 22:11:48 -0500

    Continued About.js write up

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -7,15 +7,19 @@ export default function About() {
 			<div id='written'>
 				<span id='intro'>Nice to meet you!</span>
 				<p>
-					I'm Kevin, a diligent programmer with an aptitude for creative coding from Milton, Ontario. I've been making web and software programs since I first began coding on Notepad in 2011.
+					I'm Kevin, a diligent programmer with an aptitude for creative coding since 2011 from Milton, Ontario.
 					<br />
-					From individual web pages to full scale implementations, I focus on creating captivating, accessible, and interactive applications.
+					From individual web pages to full scale implementations, I focus on creating visually captivating, accessible, and interactive applications.
 					<br /><br />
-					As a developer, I'm driven by a passion to understand and come up with solutions to challenges new and old, easy or difficult.
+					As a developer, I'm driven by my passion to understand and come up with solutions to challenges new and old.
 					<br />
-					I work well in teams, but also appreciate the space to work out my own answers.
+					I get along well in a team, but also appreciate the space to work out my own answers.
 					<br />
-					As a person, I'm quick to obsess over new hobbies,
+					As a person, I'm easygoing, optimistic, and determined. I strive to improve the efficiency and meaning behind my actions for everything I do.
+					<br />
+					I value honesty, consistency, and constructiveness, willing to both give and take criticism for continual improvement.
+					<br /><br />
+					In my downtime, I enjoy playing games, watching anime, working on personal projects, and diving into my latest hobby (recently Magic: The Gathering).
 				</p>
 			</div>
 			<div id='jots'>
