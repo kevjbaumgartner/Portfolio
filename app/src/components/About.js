43bb7a93687b519267fa85f23506a19e3d7b2672
Kevin Baumgartner <kevjbaumgartner@gmail.com>
2022-03-09 17:01:59 -0500

    Another About.js write up revision

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -8,18 +8,13 @@ export default function About() {
 				<span id='intro'>Nice to meet you!</span>
 				<p>
 					I'm Kevin, a diligent programmer with an aptitude for creative coding since 2011 from Milton, Ontario.
-					<br />
 					From individual web pages to full scale implementations, I focus on creating visually captivating, accessible, and interactive applications.
 					<br /><br />
-					As a developer, I'm driven by my passion to understand and come up with solutions to challenges new and old.
-					<br />
-					I get along well in a team, but also appreciate the space to work out my own answers.
-					<br />
-					As a person, I'm easygoing, optimistic, and determined. I strive to improve the efficiency and meaning behind my actions for everything I do.
-					<br />
-					I value honesty, consistency, and constructiveness, willing to both give and take criticism for continual improvement.
+					As a developer, my passion to understand and come up with solutions drives me to take on challenges new or old.
+					As a person, I'm laid-back, easily enthused, and always looking for the best in every situation. I take time to smell the roses, value positive competition, and strive for continuous improvement.
 					<br /><br />
-					In my downtime, I enjoy playing games, watching anime, working on personal projects, and diving into my latest hobby (recently Magic: The Gathering).
+					In my downtime, you can catch me playing games, watching anime, working on personal projects, or diving into my latest hobby (recently Magic: The Gathering).
+					If you think we'd get along, feel free to send a message my way, I'm always happy to meet new people!
 				</p>
 			</div>
 			<div id='jots'>
