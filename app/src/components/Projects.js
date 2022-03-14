5b542db1744a23b82eef6ba66eb162b3312e7bf0
Kevin Baumgartner <kevjbaumgartner@gmail.com>
2022-03-14 18:26:12 -0400

    Mild CSS modularity change
    Also adding content to the Projects.js page.

--- app/src/components/Projects.js
+++ app/src/components/Projects.js
@@ -9,7 +9,7 @@ export default function Projects() {
 					<img className='tile-img' src={require('../assets/tile-portfolio.jpg')} alt=''></img>
 					<span className='tile-title'>Portfolio</span>
 					<p className='tile-text'>
-						My portfolio, remade with React.js. It uses framework proprietary extensions
+						This portfolio made with React! It uses framework proprietary extensions
 						such as the React router and transition group to streamline interactive development.
 					</p>
 					<div className='tile-footer'>
@@ -23,10 +23,10 @@ export default function Projects() {
 				</div>
 				<div className='tile'>
 					<img className='tile-img' src={require('../assets/tile-mtgdeckbuilder.jpg')} alt=''></img>
-					<span className='tile-title'>MTG-DeckBuilder</span>
+					<span className='tile-title'>MTG Deck Builder</span>
 					<p className='tile-text'>
-						Deck builder for the Magic: The Gathering trading card game. Uses the Scryfall card
-						database API for queries, and React for the frontend.
+						Deck builder for the Magic: The Gathering trading card game. Uses the <a className='focus-link' href='https://scryfall.com/docs/api' target='_blank' rel="noreferrer">Scryfall card
+							database API</a> for queries, and React for the front end.
 					</p>
 					<div className='tile-footer'>
 						<ul className='tile-stack'>
@@ -36,6 +36,24 @@ export default function Projects() {
 							<li><i className="fa-solid fa-cloud"></i></li>
 						</ul>
 						<a className='tile-link' href='https://github.com/kevjbaumgartner/MTG-DeckBuilder' target='_blank' rel='noreferrer'><i className="fa-solid fa-link"></i> Link</a>
+					</div>
+				</div>
+				<div className='tile'>
+					<img className='tile-img' src={require('../assets/tile-spacestagram.jpg')} alt=''></img>
+					<span className='tile-title'>Spacestagram</span>
+					<p className='tile-text'>
+						Social media style photo browser for Mars rovers.
+						Made for the winter 2022 Shopify front end developer challenge. Made using vanilla
+						technologies and the <a className='focus-link' href='https://api.nasa.gov/' target='_blank' rel="noreferrer">NASA rover photo API</a>.
+					</p>
+					<div className='tile-footer'>
+						<ul className='tile-stack'>
+							<li><i className="fa-brands fa-html5"></i></li>
+							<li><i className="fa-brands fa-css3-alt"></i></li>
+							<li><i class="fa-brands fa-js"></i></li>
+							<li><i className="fa-solid fa-cloud"></i></li>
+						</ul>
+						<a className='tile-link' href='https://github.com/kevjbaumgartner/Shopify-2022-FED-IC' target='_blank' rel='noreferrer'><i className="fa-solid fa-link"></i> Link</a>
 					</div>
 				</div>
 			</div>
